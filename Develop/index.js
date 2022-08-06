d36447ab38d3eef8478a4407e385bac56dd4ad01
Garen <garendemirdogen1@gmail.com>
2022-08-05 23:51:36 -0400

    added first prompt, fs const, link file to generate page

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,20 @@
 // Packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+
+// link to page where README.md is printed
+const generatePage = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "github",
+      message: "What is your Github username?",
+    },
+  ]);
+};
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
